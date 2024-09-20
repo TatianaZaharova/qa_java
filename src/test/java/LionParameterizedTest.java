53b13ad142dff3bb815bf33f77eda8d6098ee3b9
Zakharova Tatiana <tatianaassoum123@gmail.com>
2024-09-20 17:55:33 +0300

    Проект 6 спринта

--- src/test/java/LionParameterizedTest.java
+++ src/test/java/LionParameterizedTest.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class LionParameterizedTest {
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
+@RunWith(Parameterized.class)
+public class LionParameterizedTest {
+    private String sex;
+    private boolean hasMane;
+
+
+    public LionParameterizedTest(String sex, boolean hasMane) {
+        this.sex = sex;
+        this.hasMane = hasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][] {
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+
+    @Test
+    public void lionParameterizedTest() throws Exception {
+        Feline feline = new Feline();
+        Lion lion = new Lion(sex, feline);
+        Assert.assertEquals(hasMane, lion.doesHaveMane());
+    }
 }
