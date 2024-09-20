5cfab2d63c6537f6c2611ba0091afd7979946024
Zakharova Tatiana <tatianaassoum123@gmail.com>
2024-09-20 17:55:33 +0300

    Коммит отчет jacoco

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class FelineTest {
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FelineTest {
+
+    @Spy
+    Feline feline;
+
+    @Test
+    public void eatMeatFelineTest() throws Exception {
+        feline.eatMeat();
+        Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
+    }
+
+    @Test
+    public void getFamilyFelineTest() {
+        Feline feline = new Feline();
+        Assert.assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getKittensFelineTest() {
+        feline.getKittens();
+        Mockito.verify(feline, Mockito.times(1)).getKittens(1);
+    }
+
+    @Test
+    public void getKittensFelineTestWithParameters() {
+        Feline feline = new Feline();
+        Assert.assertEquals(5, feline.getKittens(5));
+    }
 }
